6c27c6e192ea10cf80d0cd9b95cc22c133c69504
Bernhard Paus Graesdal <bernhardpg@gmail.com>
2021-12-12 18:37:31 +0100

    save wip

--- include/anymal_controller.hpp
+++ include/anymal_controller.hpp
@@ -3,6 +3,12 @@
 #include <gazebo/gazebo.hh>
 #include <gazebo/physics/physics.hh>
 
+#include <thread>
+#include "ros/ros.h"
+#include "ros/callback_queue.h"
+#include "ros/subscribe_options.h"
+#include "std_msgs/Float32.h"
+
 #include <Eigen/Core>
 
 namespace gazebo
@@ -23,6 +29,14 @@ namespace gazebo
 
 			std::string model_name;
 
+			/// \brief A node use for ROS transport
+			std::unique_ptr<ros::NodeHandle> rosNode;
+			/// \brief A ROS subscriber
+			ros::Subscriber rosSub;
+			/// \brief A ROS callbackqueue that helps process messages
+			ros::CallbackQueue rosQueue;
+			/// \brief A thread the keeps running the rosQueue
+			std::thread rosQueueThread;
   };
 
   // Tell Gazebo about this plugin, so that Gazebo can call Load on this plugin.
