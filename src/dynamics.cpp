e1283a3d8cb60053d780e6ff703c43d510f58bc4
Bernhard Paus Graesdal <bernhardpg@gmail.com>
2021-12-16 22:55:04 +0100

    Create pinocchio example code

--- src/dynamics.cpp
+++ src/dynamics.cpp
@@ -1,17 +1,33 @@
-#include "pinocchio/parsers/sample-models.hpp"
+#include "pinocchio/parsers/urdf.hpp"
+ 
 #include "pinocchio/algorithm/joint-configuration.hpp"
-#include "pinocchio/algorithm/rnea.hpp"
+#include "pinocchio/algorithm/kinematics.hpp"
  
-int main()
+#include <iostream>
+ 
+int main(int argc, char ** argv)
 {
-  pinocchio::Model model;
-  pinocchio::buildModels::manipulator(model);
-  pinocchio::Data data(model);
+  const std::string urdf_filename = argv[1];
+  
+  // Load the urdf model
+	pinocchio::Model model;
+  pinocchio::urdf::buildModel(urdf_filename, model);
+  std::cout << "model name: " << model.name << std::endl;
+  
+  // Create data required by the algorithms
+	pinocchio::Data data(model);
+  
+  // Sample a random configuration
+  Eigen::VectorXd q = pinocchio::randomConfiguration(model);
+  std::cout << "q: " << q.transpose() << std::endl;
  
-  Eigen::VectorXd q = pinocchio::neutral(model);
-  Eigen::VectorXd v = Eigen::VectorXd::Zero(model.nv);
-  Eigen::VectorXd a = Eigen::VectorXd::Zero(model.nv);
+	pinocchio::forwardKinematics(model,data,q);
  
-  const Eigen::VectorXd & tau = pinocchio::rnea(model,data,q,v,a);
-  std::cout << "tau = " << tau.transpose() << std::endl;
-}
+  // Print out the placement of each joint of the kinematic tree
+  for(pinocchio::JointIndex joint_id = 0; joint_id < (pinocchio::JointIndex) model.njoints; ++joint_id)
+    std::cout << std::setw(24) << std::left
+              << model.names[joint_id] << ": "
+              << std::fixed << std::setprecision(2)
+              << data.oMi[joint_id].translation().transpose()
+              << std::endl;
+} 
