2322f8a2eb16afb582ada7bf5e8cb9acc0d9bec3
Bernhard Paus Graesdal <bernhardpg@gmail.com>
2021-12-17 01:46:31 +0100

    Implement test calculation of inertia matrix, coriolis matrix and gravity vector

--- src/dynamics.cpp
+++ src/dynamics.cpp
@@ -2,6 +2,7 @@
  
 #include "pinocchio/algorithm/joint-configuration.hpp"
 #include "pinocchio/algorithm/kinematics.hpp"
+#include "pinocchio/algorithm/compute-all-terms.hpp"
  
 #include <iostream>
  
@@ -11,23 +12,36 @@ int main(int argc, char ** argv)
   
   // Load the urdf model
 	pinocchio::Model model;
-  pinocchio::urdf::buildModel(urdf_filename, model);
+  pinocchio::urdf::buildModel(
+			urdf_filename, pinocchio::JointModelFreeFlyer(), model
+			); // Specify JointModelFreeFlyer to make root joint floating base
   std::cout << "model name: " << model.name << std::endl;
   
   // Create data required by the algorithms
 	pinocchio::Data data(model);
+
+	std::cout << "	nq: " << model.nq << std::endl;
+	std::cout << "	nv: " << model.nv << std::endl;
+
+	Eigen::VectorXd q(19);
+	q << 0, 0, 0,  // fb pos
+			 1, 0, 0, 0, // fb attitude
+			 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0; // joints
+
+	Eigen::VectorXd v(18);
+	v << 0, 0, 0, // fb linear vel
+		   0, 0, 0, // fb ang vel
+			 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0; // joints
+
+	pinocchio::computeAllTerms(model, data, q, v);
+
+	std::cout << "Joint space inertia matrix:\n";
+	std::cout << data.M << std::endl << std::endl;
+
+	std::cout << "Coriolis matrix:\n";
+	std::cout << data.C << std::endl << std::endl;
+
+	std::cout << "Gravity vector:\n";
+	std::cout << data.g << std::endl;
   
-  // Sample a random configuration
-  Eigen::VectorXd q = pinocchio::randomConfiguration(model);
-  std::cout << "q: " << q.transpose() << std::endl;
- 
-	pinocchio::forwardKinematics(model,data,q);
- 
-  // Print out the placement of each joint of the kinematic tree
-  for(pinocchio::JointIndex joint_id = 0; joint_id < (pinocchio::JointIndex) model.njoints; ++joint_id)
-    std::cout << std::setw(24) << std::left
-              << model.names[joint_id] << ": "
-              << std::fixed << std::setprecision(2)
-              << data.oMi[joint_id].translation().transpose()
-              << std::endl;
 } 
