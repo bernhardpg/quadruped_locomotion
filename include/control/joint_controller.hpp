7e3846b6ba3b196c79f6331d828b4134ee11ae8d
Bernhard Paus Graesdal <bernhardpg@gmail.com>
2021-12-23 16:55:32 +0100

    Create a joint controller node

---
+++ include/control/joint_controller.hpp
@@ -0,0 +1,62 @@
+#include <ros/ros.h>
+#include <ros/console.h>
+#include <std_msgs/Float64MultiArray.h>
+#include <Eigen/Core>
+#include <eigen_conversions/eigen_msg.h>
+
+namespace control
+{
+	const int kNumJoints = 12;
+	const int kNumGenCoords = 19;
+	const int kNumGenVels = 18;
+
+	class JointController
+	{
+		public:
+			JointController();
+			JointController(int frequency_);
+
+		private:
+			int frequency_;
+
+			std::string model_name_;
+
+			Eigen::Matrix<double, kNumJoints, 1> q_j_;
+			Eigen::Matrix<double, kNumJoints, 1> q_j_dot_;
+			Eigen::Matrix<double, kNumJoints, 1> q_j_cmd_;
+			Eigen::Matrix<double, kNumJoints, 1> q_j_dot_cmd_;
+			Eigen::Matrix<double, kNumJoints, 1> tau_cmd_;
+
+			double k_joints_p_= 0.1; // TODO: Tune these
+			double k_joints_d_ = 0.1; // TODO: Tune these
+			void CalcJointTorques();
+
+			// *** //
+			// ROS //
+			// *** //
+
+			void InitRos();
+
+			ros::NodeHandle node_handle_;
+			ros::Rate loop_rate_;
+
+			ros::Publisher torque_cmd_pub_;
+			ros::Subscriber q_joint_cmd_sub_;
+			ros::Subscriber q_joint_dot_cmd_sub_;
+			ros::Subscriber gen_coord_sub_;
+			ros::Subscriber gen_vel_sub_;
+
+			void OnGenCoordMsg(
+					const std_msgs::Float64MultiArrayConstPtr &msg
+					);
+			void OnGenVelMsg(
+					const std_msgs::Float64MultiArrayConstPtr &msg
+					);
+			void OnJointPosCmdMsg(
+					const std_msgs::Float64MultiArrayConstPtr &msg
+					);
+			void OnJointVelCmdMsg(
+					const std_msgs::Float64MultiArrayConstPtr &msg
+					);
+	};
+}
---
+++ src/control/joint_controller.cpp
@@ -0,0 +1,130 @@
+#include "control/joint_controller.hpp"
+
+namespace control
+{
+	JointController::JointController()
+		: JointController(2500)
+	{}
+
+	JointController::JointController(int frequency_)
+		: loop_rate_(frequency_)
+	{
+		ROS_INFO("Starting joint controller at %d Hz", frequency_);
+		model_name_ = "anymal"; // TODO: Take model name as an argument to the node
+
+		q_j_.setZero();
+		q_j_dot_.setZero();
+		q_j_cmd_.setZero();
+		q_j_dot_cmd_.setZero();
+		tau_cmd_.setZero();
+
+		InitRos();
+	}
+
+	void JointController::InitRos()
+	{
+		int argc = 0;
+		char **argv = NULL;
+
+		torque_cmd_pub_ = node_handle_
+			.advertise<std_msgs::Float64MultiArray>(
+					"/" + model_name_ + "/torque_cmd", 1
+					);
+
+		q_joint_cmd_sub_ = node_handle_
+			.subscribe<std_msgs::Float64MultiArray>(
+					"/q_j_cmd", 1,
+					boost::bind(&JointController::OnJointPosCmdMsg, this, _1)
+					);
+
+		q_joint_dot_cmd_sub_ = node_handle_
+			.subscribe<std_msgs::Float64MultiArray>(
+					"/q_j_dot_cmd", 1,
+					boost::bind(&JointController::OnJointVelCmdMsg, this, _1)
+					);
+
+		gen_coord_sub_ = node_handle_
+			.subscribe<std_msgs::Float64MultiArray>(
+					"/gen_coord", 1,
+					boost::bind(&JointController::OnGenCoordMsg, this, _1)
+					);
+
+		gen_vel_sub_ = node_handle_
+			.subscribe<std_msgs::Float64MultiArray>(
+					"/gen_vel", 1,
+					boost::bind(&JointController::OnGenVelMsg, this, _1)
+					);
+
+		// Single threaded
+		while(ros::ok())
+		{
+			ros::spinOnce(); // spin once to fetch newest messages
+			CalcJointTorques();
+
+			std_msgs::Float64MultiArray torque_cmd_msg;
+			tf::matrixEigenToMsg(tau_cmd_, torque_cmd_msg);
+
+			torque_cmd_pub_.publish(torque_cmd_msg);
+
+			ros::spinOnce(); // publish messages
+			loop_rate_.sleep();
+		}
+	}
+
+	void JointController::CalcJointTorques()
+	{
+		tau_cmd_ = k_joints_p_ * (q_j_cmd_ - q_j_)
+			+ k_joints_d_ * (q_j_dot_cmd_ - q_j_dot_);
+					
+	}
+
+	void JointController::OnGenCoordMsg(
+			const std_msgs::Float64MultiArrayConstPtr &msg
+			)
+	{
+		for (size_t i = kNumGenCoords - kNumJoints; i < kNumGenCoords; ++i)
+		{
+			q_j_(i)	 = msg->data[i]; // Only store joint positions
+		}
+	}
+
+	void JointController::OnGenVelMsg(
+			const std_msgs::Float64MultiArrayConstPtr &msg
+			)
+	{
+		for (size_t i = kNumGenVels - kNumJoints; i < kNumGenVels; ++i)
+		{
+			q_j_dot_(i)	 = msg->data[i]; // Only store joint positions
+		}
+	}
+
+	void JointController::OnJointPosCmdMsg(
+			const std_msgs::Float64MultiArrayConstPtr &msg
+			)
+	{
+		for (size_t i = 0; i < kNumJoints; ++i)
+		{
+			q_j_cmd_(i)	 = msg->data[i];
+		}
+	}
+
+	void JointController::OnJointVelCmdMsg(
+			const std_msgs::Float64MultiArrayConstPtr &msg
+			)
+	{
+		for (size_t i = 0; i < kNumJoints; ++i)
+		{
+			q_j_dot_cmd_(i)	 = msg->data[i];
+		}
+	}
+}
+
+int main(int argc, char **argv)
+{
+	ros::init(argc, argv, "joint_controller",
+		ros::init_options::NoSigintHandler
+		);
+	int frequency = 2500;
+	control::JointController joint_controller_(2500);
+	return 0;
+}
