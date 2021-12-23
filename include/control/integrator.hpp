4d8f284cd39861cc5feeca8fc754efd0612b4b14
Bernhard Paus Graesdal <bernhardpg@gmail.com>
2021-12-23 15:40:36 +0100

    Make an integrator object

---
+++ include/control/integrator.hpp
@@ -0,0 +1,26 @@
+#include "ros/ros.h"
+#include "ros/console.h"
+#include <Eigen/Core>
+
+//TODO: Move this to another folder when used by anything else than controller
+
+class Integrator
+{
+	public:
+		Integrator(){};
+		Integrator(int rows);
+
+		void Integrate(Eigen::VectorXd vec);
+		void SetSize(int rows);
+		void Reset();
+
+		Eigen::VectorXd GetIntegral();
+		void SetIntegral(Eigen::VectorXd target);
+
+	private:
+		double dt_;
+		ros::Time last_timestamp_;
+		Eigen::VectorXd integral_;
+
+		double GetElapsedTimeSince(ros::Time t);
+};
---
+++ src/control/integrator.cpp
@@ -0,0 +1,38 @@
+#include "control/integrator.hpp"
+
+Integrator::Integrator(int rows)
+{
+	SetSize(rows);
+	Reset();
+}
+
+void Integrator::SetSize(int rows)
+{
+	integral_.resize(rows);
+}
+
+void Integrator::Reset()
+{
+	integral_.setZero();
+}
+
+Eigen::VectorXd Integrator::GetIntegral()
+{
+	return integral_;
+}
+
+void Integrator::SetIntegral(Eigen::VectorXd target)
+{
+	integral_ = target;
+}
+
+void Integrator::Integrate(Eigen::VectorXd vec)
+{
+	integral_ += dt_ * vec;
+}
+
+double Integrator::GetElapsedTimeSince(ros::Time t)
+{
+	double elapsed_time = (ros::Time::now() - t).toSec();
+	return elapsed_time; 
+}
