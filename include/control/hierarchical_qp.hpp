bb1644905717d19af6bc0a8c8a49a7eba05fb1ea
Bernhard Paus Graesdal <bernhardpg@gmail.com>
2021-12-28 18:10:28 +0100

    save wip

--- include/control/hierarchical_qp.hpp
+++ include/control/hierarchical_qp.hpp
@@ -11,26 +11,46 @@
 
 namespace control
 {
+	const double kInf = 9999999; // TODO: move to another file
+
 	class HierarchicalQP
 	{
 		public:
 			HierarchicalQP();
+			HierarchicalQP(int num_tasks);
 			~HierarchicalQP(){};
 
 		private:
-			drake::solvers::MathematicalProgram prog_;
-
+			int num_tasks_;
+			int num_contacts_;
 			int	num_decision_vars_;
-			symbolic_vector_t u_dot_;	
-			symbolic_vector_t lambda_;	
 
+			std::vector<std::unique_ptr<
+				drake::solvers::MathematicalProgram>
+				> quadratic_progs_;
+			std::vector<symbolic_vector_t> decision_variables_;
+			std::vector<Eigen::MatrixXd> eq_const_matrices_;
+			std::vector<Eigen::VectorXd> eq_const_vectors_;
+			std::vector<Eigen::MatrixXd> ineq_const_matrices_;
+			std::vector<Eigen::VectorXd> ineq_const_vectors_;
+			std::vector<symbolic_vector_t> slack_variables_;
+
+			void PopulateVariables(); // TODO: Rename or replace 
+			void CreateQPWithIndex(int index);
+			void CreateDecisionVariables();
 			void AddEqConstraint(
-					Eigen::MatrixXd A, Eigen::VectorXd b
+					Eigen::MatrixXd A,
+					Eigen::VectorXd b,
+					symbolic_vector_t decision_variables
 					);
 			void AddIneqConstraint(
-					Eigen::MatrixXd D, Eigen::VectorXd f
+					Eigen::MatrixXd D,
+					Eigen::VectorXd f,
+					symbolic_vector_t decision_variables
 					);
 
-			symbolic_vector_t GetDecisionVars();
+			Eigen::VectorXd CreateInfVector(int size);
+
+			void Test();
 	};
 }
--- src/control/hierarchical_qp.cpp
+++ src/control/hierarchical_qp.cpp
@@ -3,54 +3,124 @@
 namespace control
 {
 	HierarchicalQP::HierarchicalQP()
+		: HierarchicalQP(3)
+	{}
+
+	HierarchicalQP::HierarchicalQP(int num_tasks)
+		: num_tasks_(num_tasks),
+			decision_variables_(num_tasks_),
+			 eq_const_matrices_(num_tasks_),
+			 eq_const_vectors_(num_tasks_),
+			 ineq_const_matrices_(num_tasks_),
+			 ineq_const_vectors_(num_tasks_),
+			 slack_variables_(num_tasks_)
 	{
-		int num_contacts = 4; // TODO: generalize this
+		num_contacts_ = 4; // TODO: generalize this
 
-		u_dot_ = prog_.NewContinuousVariables(kNumGenVels, 1, "u_dot");
-		lambda_ = prog_.
-			NewContinuousVariables(kNumPosDims * num_contacts, 1, "lambda");
-		num_decision_vars_ = u_dot_.rows() + lambda_.rows();
+		num_decision_vars_ = kNumGenVels + kNumPosDims * num_contacts_;
+		PopulateVariables();
+		CreateQPWithIndex(0);
+		//CreateDecisionVariables();
+	}
 
+	void HierarchicalQP::CreateDecisionVariables()
+	{
+//		for (int task_i = 0; task_i < num_tasks_; ++task_i)
+//		{
+//			symbolic_vector_t u_dot = quadratic_progs_[task_i]
+//				.NewContinuousVariables(kNumGenVels, 1, "u_dot");
+//			symbolic_vector_t lambda = quadratic_progs_[task_i]
+//				.NewContinuousVariables(kNumPosDims * num_contacts_, 1, "lambda");
+//			symbolic_vector_t task_i_vars(num_decision_vars_);
+//			task_i_vars << u_dot, lambda;
+//		}
+	}
+
+	void HierarchicalQP::PopulateVariables()
+	{
+		// TODO: Just placeholder test code
+		Eigen::MatrixXd A = Eigen::MatrixXd::Identity(num_decision_vars_, num_decision_vars_);
+		eq_const_matrices_[0] = A;
+		eq_const_matrices_[1] = A;
+		eq_const_matrices_[2] = A;
+
+		ineq_const_matrices_[0] = A;
+		ineq_const_matrices_[1] = A;
+		ineq_const_matrices_[2] = A;
+
+		Eigen::MatrixXd b = Eigen::MatrixXd::Zero(num_decision_vars_,1);
+		for (int i = 0; i < num_decision_vars_; ++i)
+			b(i) = (double) i;
+
+		eq_const_vectors_[0] = b;
+		eq_const_vectors_[1] = b;
+		eq_const_vectors_[2] = b;
+		ineq_const_vectors_[0] = b;
+		ineq_const_vectors_[1] = b;
+		ineq_const_vectors_[2] = b;
+	}
+
+	void HierarchicalQP::CreateQPWithIndex(int index)
+	{
+		// TODO: How to make a new prog every time?
+		drake::solvers::MathematicalProgram new_prog;
+
+		symbolic_vector_t u_dot = new_prog
+			.NewContinuousVariables(kNumGenVels, 1, "u_dot");
+		symbolic_vector_t lambda = new_prog
+			.NewContinuousVariables(kNumPosDims * num_contacts_, 1, "lambda");
+		symbolic_vector_t task_i_vars(num_decision_vars_);
+		task_i_vars << u_dot, lambda;
+	}
+
+	symbolic_vector_t HierarchicalQP::Create
+
+	void HierarchicalQP::AddEqConstraint(
+			Eigen::MatrixXd A,
+			Eigen::VectorXd b,
+			symbolic_vector_t decision_variables
+			)
+	{
+//		symbolic_vector_t constraint = A * decision_variables - b;
+//		auto squared_constraint = 0.5 * constraint.transpose() * constraint;
+//		prog_.AddQuadraticCost(squared_constraint);
+	}
+
+	void HierarchicalQP::AddIneqConstraint(
+			Eigen::MatrixXd D,
+			Eigen::VectorXd f,
+			symbolic_vector_t decision_variables
+			)
+	{
+//		int num_rows = 	D.rows(); // TODO: is this a nice way to get number of rows?
+//
+//		symbolic_vector_t new_vs = prog_.NewContinuousVariables(num_rows, 1, "v");
+//
+//		symbolic_vector_t constraint = D * decision_variables - f - new_vs;
+//		Eigen::VectorXd zero_vec = Eigen::VectorXd::Zero(num_rows);
+//		Eigen::VectorXd inf_vec = CreateInfVector(num_rows);
+//
+//		prog_.AddLinearConstraint(constraint, -inf_vec, zero_vec);
+		//prog_.AddQuadraticCost(0.5 * new_vs.transpose() * new_vs);
+	}
+
+	Eigen::VectorXd HierarchicalQP::CreateInfVector(int size)
+	{
+		Eigen::VectorXd inf_vec = Eigen::VectorXd::Zero(size);
+		for (int i = 0; i < size; ++i)
+			inf_vec(i) = kInf;
+
+		return inf_vec;
+	}
+
+	// TODO: delete
+	void HierarchicalQP::Test()
+	{
 		// TEST
 		Eigen::MatrixXd A = Eigen::MatrixXd::Identity(num_decision_vars_, num_decision_vars_);
 		
 		Eigen::MatrixXd b = Eigen::MatrixXd::Zero(num_decision_vars_,1);
 		for (int i = 0; i < num_decision_vars_; ++i)
 			b(i) = (double) i;
-
-		//AddEqConstraint(A,b);
-		AddIneqConstraint(A,b);
-	}
-
-	symbolic_vector_t HierarchicalQP::GetDecisionVars()
-	{
-		symbolic_vector_t decision_variables(num_decision_vars_);
-		decision_variables << u_dot_, lambda_;
-		return decision_variables;
-	}
-
-	void HierarchicalQP::AddEqConstraint(
-			Eigen::MatrixXd A, Eigen::VectorXd b
-			)
-	{
-		symbolic_vector_t constraint = A * GetDecisionVars() - b;
-		auto squared_constraint = 0.5 * constraint.transpose() * constraint;
-		prog_.AddQuadraticCost(squared_constraint);
-	}
-
-	void HierarchicalQP::AddIneqConstraint(
-			Eigen::MatrixXd D, Eigen::VectorXd f
-			)
-	{
-		int num_rows = 	D.rows(); // TODO: is this a nice way to get number of rows?
-
-		symbolic_vector_t new_vs = prog_.NewContinuousVariables(num_rows, 1, "v");
-
-		symbolic_vector_t constraint = D * GetDecisionVars() - f - new_vs;
-		Eigen::VectorXd zero_vec = Eigen::VectorXd::Zero(num_rows);
-		Eigen::VectorXd inf_vec = Eigen::VectorXd::Zero(num_rows);
-		for (int i = 0; i < num_rows; ++i)
-			inf_vec(i) = 999999;
-		prog_.AddLinearConstraint(constraint, -inf_vec, zero_vec);
 	}
 }
